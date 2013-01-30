1464907e6cb9b1628457d13c926f5d0455c60235
somasekhar <sekhar.sekhar9@gmail.com>
2013-01-30 17:29:33 +0530

    add

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
 
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @products }
